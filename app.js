e364c15aed614c248bafa919c0adfb20c549eba8
Marie <marie.vaba@gmail.com>
2021-10-14 09:01:56 +0300

    Teksti väljastamine konsooli ja kommenteerimine

---
+++ app.js
@@ -0,0 +1,14 @@
+//teksti väljastamine
+//kommentaaride kasutamine
+
+//üherealine kommentaar
+
+/*
+mitme
+realine
+kommentaar
+*/
+
+console.log("Tere Maailm")
+console.log("See on teine rida")
+console.log('See ' + 'on ' + 'liitlause')
