6c2f8e8610a7362d510e71df3663a4450244d093
Marie <marie.vaba@gmail.com>
2021-11-03 19:03:39 +0200

    if-else

---
+++ iseseisev/jäätumine/jaatumine.js
@@ -0,0 +1,8 @@
+// kasutaja sisend
+ let temperatuur = prompt('Sisesta õhutemperatuur:')
+//tingimuslause
+if (temperatuur <= 4.0) {
+    console.log('Ei ole jäätumise ohtu')
+} else {
+    console.log('On jäätumise oht')
+}
